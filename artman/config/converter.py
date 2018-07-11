d96a1ad829a0775903be1435cafe91fcc3730dbe
Andrea Lin <asdf.lina@gmail.com>
2018-07-11 16:08:27 -0700

    make src_proto_paths optional

--- artman/config/converter.py
+++ artman/config/converter.py
@@ -36,8 +36,10 @@ def convert_to_legacy_config_dict(artifact_config, root_dir, output_dir):
     common['organization_name'] = artifact_config.organization_name
     common['service_yaml'] = artifact_config.service_yaml
     common['gapic_yaml'] = artifact_config.gapic_yaml
-    common['src_proto_path'], excluded_proto_path = _calculate_proto_paths(
-        artifact_config_dict['src_proto_paths'])
+    excluded_proto_path = None
+    if 'proto_deps' in artifact_config_dict:
+        common['src_proto_path'], excluded_proto_path = _calculate_proto_paths(
+            artifact_config_dict['src_proto_paths'])
     if excluded_proto_path:
         common['excluded_proto_path'] = excluded_proto_path
     common['import_proto_path'] = [root_dir]
